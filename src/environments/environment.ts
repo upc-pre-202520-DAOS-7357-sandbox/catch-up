e22c877bc0b6a82b00439732e72265ceee69c5b2
Rafael C <rafael.castro@upc.edu.pe>
2025-09-18 15:07:20 -0500

    chore: add environment files inside src/environments.

---
+++ src/environments/environment.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  production: true,
+  newsProviderApiBaseUrl: 'https://newsapi.org/v2',
+  newsProviderNewsEndpointPath: '/top-headlines',
+  newsProviderSourcesEndpointPath: '/sources',
+  newsProviderApiKey: '',
+  logoProviderApiBaseUrl: 'https://logo.clearbit.com/'
+};
